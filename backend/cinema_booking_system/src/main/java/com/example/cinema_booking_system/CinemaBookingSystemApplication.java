adb6f883512d0c7d03ae85e5bc01a5556757163b
przemek06 <swiatprzemyslaw@gmail.com>
2023-04-07 13:12:17 +0200

    init

---
+++ backend/cinema_booking_system/src/main/java/com/example/cinema_booking_system/CinemaBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinema_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaBookingSystemApplication.class, args);
+	}
+
+}
