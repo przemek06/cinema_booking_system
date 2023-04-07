f903e9c3e3a0389e6209361328e8db50be4fa240
przemek06 <swiatprzemyslaw@gmail.com>
2023-04-07 13:27:42 +0200

    replace backend

---
+++ backend/cinema_booking_api/src/main/java/pwr/web/cinema_booking_api/CinemaBookingApiApplication.java
@@ -0,0 +1,13 @@
+package pwr.web.cinema_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBookingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaBookingApiApplication.class, args);
+    }
+
+}
