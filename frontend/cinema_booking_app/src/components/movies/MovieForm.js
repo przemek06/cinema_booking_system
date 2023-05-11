4d1c672f0bea0be7c221cb02b91ec1f9086b36ca
ninjvv <nina.chwastowska@gmail.com>
2023-05-11 18:41:47 +0200

    add input fields for 'character' and 'actor'

--- frontend/cinema_booking_app/src/components/movies/MovieForm.js
+++ frontend/cinema_booking_app/src/components/movies/MovieForm.js
@@ -1,52 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import "../Form.css";
 import SubmitButton from '../buttons/SubmitButton';
-import MultipleValueTextInput from 'react-multivalue-text-input';
 
 
 export default function MovieForm() {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    // const onSubmit = data => addMovie(data, navigate);
-    const navigate  = useNavigate()
-    console.log(errors);
-    const characters = [];
+    const navigate = useNavigate();
+    const [inputCount, setInputCount] = useState(0);
+    const [inputValues, setInputValues] = useState({});
+
+    const addInput = () => {
+        setInputCount(prevCount => prevCount + 1);
+        setInputValues(prevValues => ({
+            ...prevValues,
+            [`character${inputCount + 1}`]: '',
+            [`actor${inputCount + 1}`]: '',
+        }));
+    };
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setInputValues(prevValues => ({
+            ...prevValues,
+            [name]: value,
+        }));
+    };
+
+    const onSubmit = (data) => {
+        // Perform form submission logic here
+        data.characters = Object.values(inputValues);
+        console.log(data);
+        navigate('/'); // Example: Navigate to a different page after submission
+    };
 
     return (
         <div>
         <h1>Add movie</h1>
-        <form onSubmit={null}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <label className='form-text'>Title</label>
-            <input className='form-field' type="text" {...register("Title", {required: true})} />
-            <br/>
+            <input className='form-field' type="text" {...register("Title", { required: true })} />
+            <br />
             <label className='form-text'>Category</label>
-            <input className='form-field' type="text" {...register("Category", {required: true})} />
-            <br/>
+            <input className='form-field' type="text" {...register("Category", { required: true })} />
+            <br />
             <label className='form-text'>Overview</label>
-            <textarea rows={5} className='form-field' type="text" {...register("Overview", {required: true})} />
-            <br/>
+            <textarea rows={5} className='form-field' {...register("Overview", { required: true })} />
+            <br />
             <label className='form-text'>Description</label>
-            <textarea rows={10} className='form-field' type="text" {...register("Description", {required: true})} />
-            <br/>
+            <textarea rows={10} className='form-field' {...register("Description", { required: true })} />
+            <br />
             <label className='form-text'>Image</label>
-            <br/>
-            <input className='form-file' type="file" accept="image/png, image/jpeg" {...register("Image", {required: true})} />
-            <br/>
+            <br />
+            <input className='form-file' type="file" accept="image/png, image/jpeg" {...register("Image", { required: true })} />
+            <br />
             <label className='form-text'>Duration</label>
-            <input className='form-field' type="text" {...register("Duration", {required: true, pattern: "[0-9]+"})} />
-            <br/>
+            <input className='form-field' type="text" {...register("Duration", { required: true, pattern: "[0-9]+" })} />
+            <br />
             <label className='form-text'>Characters</label>
-            <MultipleValueTextInput
-                className='form-field'
-                onItemAdded={(item, allItems) => characters.push(item)}
-                onItemDeleted={(item, allItems) => characters.splice(characters.indexOf(item), 1)}
-                name="item-input"
-                placeholder="Enter characters; separate them with COMMA or ENTER."
-            />
-            <br/>
+            {Object.keys(inputValues).map((inputName, index) => {
+            if (inputName.startsWith('character')) {
+                const actorName = inputName.replace('character', 'actor');
+                return (
+                <div key={inputName}>
+                    <label htmlFor={inputName}>Character: </label>
+                    <input
+                    className='small-field'
+                    type="text"
+                    name={inputName}
+                    id={inputName}
+                    value={inputValues[inputName]}
+                    onChange={handleInputChange}
+                    required
+                    />
+                    <label htmlFor={actorName}>Actor: </label>
+                    <input
+                    className='small-field'
+                    type="text"
+                    name={actorName}
+                    id={actorName}
+                    value={inputValues[actorName]}
+                    onChange={handleInputChange}
+                    required
+                    />
+                </div>
+                );
+            }
+            return null;
+            })}
+            <button type="button" className='add-more' onClick={addInput}>Add character</button>
+            <br />
             <SubmitButton value="ADD MOVIE"/>
-        </form>
-        </div>
-    );
-}
+            </form>
+            </div>
+        );
+    }
