fbc40ae3dba75b811499695aae591f02612777bb
przemek06 <swiatprzemyslaw@gmail.com>
2023-05-24 21:58:28 +0200

    interface specification

--- frontend/cinema_booking_app/src/components/reservations/SeatMap.js
+++ frontend/cinema_booking_app/src/components/reservations/SeatMap.js
@@ -1,7 +1,7 @@
 
-// reservations - already taken seats
-// chosenSeats - seats chosen by the user
-// setChosenSeats - update seats chosen by the user
+// reservations - already taken seats (list of json objects in form: {seatRow: num, seatColumn: num})
+// chosenSeats - seats chosen by the user (list of json objects in form: {row: num, column: num})
+// setChosenSeats - update seats chosen by the user (delete or add new seat)
 const SeatMap = (reservations, chosenSeats, setChosenSeats) => {
 
     return (
